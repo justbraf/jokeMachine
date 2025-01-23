37df134d1128741c41e1b7e45b3c3fa29d20188c
justbraf <dwaynecorey@gmail.com>
2025-01-23 09:06:25 -0400

    count docs

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
 import express from 'express'
-import { PORT, JOKES } from './config.js'
+import { PORT, JOKES, MDBURI } from './config.js'
+import { MongoClient, ServerApiVersion } from "mongodb"
 
 const app = express()
+
+const client = new MongoClient(MDBURI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+})
+
+const myDatabase = client.db("sample_supplies")
+const myCollection = myDatabase.collection("sales")
 
 app.listen(PORT, () => {
     console.info(`Server started on port ${PORT}`)
 })
 
+app.get('/sales', (req, res) => {
+    myCollection.find().count()
+    .then(resp=>{
+        console.log(resp)
+        res.sendStatus(200)
+    })
+    .catch(err=>{
+        console.log("an error occured", err)
+        res.sendStatus(500)
+    })
+})
+
 app.get("/", (req, res) => {
     res.send("Please check documentation for usage.")
 })
+
 app.get("/joke", (req, res) => {
     let index = Math.floor(Math.random() * JOKES.length)
     res.status(200).json(JOKES[index])
 })
 
-app.get("/joke/:id", (req, res) =>{
+app.get("/joke/:id", (req, res) => {
     const jid = req.params.id
     if (isNaN(jid)) {
         res.status(200).json({
@@ -24,7 +49,7 @@ app.get("/joke/:id", (req, res) =>{
         return
     }
     const found = JOKES.find((joke) => joke.id == jid)
-    if (!found){
+    if (!found) {
         res.status(200).json({
             "error": "Invalid joke id"
         })
